4f3f3f4da34ebe04daa4cf3467e864182ca1752f
juliaelinda12 <juliaelinda988@gmail.com>
2023-03-15 22:11:09 +0700

    update pendaftaran 2

---
+++ resources/views/pendaftarans/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('pendaftarans.layout')
+     
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Product</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('pendaftarans.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+     
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> Input Gagal.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    
+    <form action="{{ route('pendaftarans.update',$pendaftaran->id) }}" method="POST" enctype="multipart/form-data"> 
+        @csrf
+        @method('PUT')
+     
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nama Website:</strong>
+                    <input type="text" name="namawebsite" value="{{ $pendaftaran->namawebsite }}" class="form-control" placeholder="Nama website">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Image:</strong>
+                    <input type="file" name="image" class="form-control" placeholder="image">
+                    <img src="/image/{{ $pendaftaran->image }}" width="300px">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>URL:</strong>
+                    <input type="text" name="url" value="{{ $pendaftaran->url }}" class="form-control" placeholder="url">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>token:</strong>
+                    <input type="text" name="token" value="{{ $pendaftaran->token }}" class="form-control" placeholder="token">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>status:</strong>
+                    <input type="radio" name="status" value="{{ $pendaftaran->status }}" id="status" selected> Publish
+                    <input type="radio" name="status"  value="{{ $pendaftaran->status }}" id="status" selected> No Publish
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+     
+    </form>
+@endsection
